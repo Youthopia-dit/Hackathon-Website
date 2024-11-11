6c2c394430e44e35514317f4fef8d15efd62274a
tanya-2002 <tanyaaggarwal2002@gmail.com>
2024-11-11 12:14:21 +0530

    final

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -16,23 +16,25 @@ function landingpage() {
             <div className="text">2024</div>
             <div className="main-text"><img className="img" src={Hack} /></div>
             <div className="text2">Hack the Change, Sustain the World</div>
-            <div className="bulign">
+            <div className="venue">
+                <div className="bulign">
                 <div className="button">WHEN?</div>
-                <div className="button">WHERE?</div>
-            </div>
-            <div className="bulign2">
                 <div className="info">12-13 November</div>
+                </div>
+                <div className="bulign2">
+                <div className="button">WHERE?</div>
                 <div className="info">DIT University</div>
+                </div>
             </div>
             <div className="bulign">
                 <button className="button2" onClick ={()=> navigate("/PrblmState")}>Problem Statements</button>
             </div>
-            <div className="wavy">
+            {/* <div className="wavy">
                 <img src={A1} alt="wave" id="wave" />
                 <img src={A2} alt="wave2" id="wave1" />
                 <img src={A3} alt="wave2" id="wave2" />
                 <img src={A4} alt="wave2" id="wave3" />
-            </div>
+            </div> */}
         </div>
     )
 }
